831900b879febbaf9078316b5cb11e78bbca4910
Gauranshi Gupta <95520163+ggauranshi-03@users.noreply.github.com>
2024-02-07 12:09:37 +0530

    navbar done

---
+++ src/Components/Pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import LeftSide from "../LifeSidebar/LeftSide";
+const Home = () => {
+  return (
+    <div className="w-full">
+      <div className="fixed top-0 z-10 w-full bg-white">
+        <Navbar />
+      </div>
+      <div className="flex bg-gray-100">
+        <div className="flex-auto w-[20%] fixed top-12">
+          <LeftSide />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
