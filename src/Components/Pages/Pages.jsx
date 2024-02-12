d3f96228fcaf3c0e01498603aeb5bcec061e21c7
Gauranshi Gupta <95520163+ggauranshi-03@users.noreply.github.com>
2024-02-12 21:23:26 +0530

    added authorization pages

--- src/Components/Pages/Pages.jsx
+++ src/Components/Pages/Pages.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Home from "./Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Register from "./Register";
+import Reset from "./Reset";
+// import FriendProfile from "./FriendProfile";
+
 const Pages = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/reset" element={<Reset />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
